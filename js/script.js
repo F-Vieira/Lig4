ae2641ab95a3fac8fb306a4de6c669ac1f4ec827
Fernando Vieira <fernando.santos2901@gmail.com>
2021-06-07 18:43:45 -0300

    packing code

--- js/script.js
+++ js/script.js
@@ -1,111 +1,109 @@
 let body = document.body;
-let container1 = document.createElement('div')
-container1.id = 'container1';
+
+let container1 = document.createElement("div");
+container1.id = "container1";
 body.appendChild(container1);
-let box = document.createElement('div')
-box.id = 'box';
+
+let box = document.createElement("div");
+box.id = "box";
 body.appendChild(box);
-let container2 = document.createElement('div')
-container2.id = 'container2';
+
+let container2 = document.createElement("div");
+container2.id = "container2";
 body.appendChild(container2);
-let line = '';
-let bloco = '';
+
+let line = "";
+let bloco = "";
+
 constructTable();
-function constructTable(){
-    for(let i = 0; i<=6; i++){
-        line = document.createElement('div');
-        line.id = 'line'+i;
-        line.classList.add('line');
-        box.appendChild(line);
-    }
+
+function constructTable() {
+  for (let i = 0; i <= 6; i++) {
+    line = document.createElement("div");
+    line.id = "line" + i;
+    line.classList.add("line");
+    box.appendChild(line);
+  }
 }
+
 /* Criando o peça preta e vermelha */
 let blocoP = document.createElement("div");
 blocoP.id = "Preto";
 container1.appendChild(blocoP);
+
 let blocoV = document.createElement("div");
 blocoV.id = "Vermelho";
 container1.appendChild(blocoV);
-let block = '';
+
+let block = "";
 let num = 0;
-let arr = [[],[],[],[],[],[],[]];
+let arr = [[], [], [], [], [], [], []];
+
 /* matriz de elementos */
-for(let i = 0; i <= 6; i++){
-    for(let j = 0; j < 6; j++){
-        arr[i][j] = 0;
-    }
+for (let i = 0; i <= 6; i++) {
+  for (let j = 0; j < 6; j++) {
+    arr[i][j] = 0;
+  }
 }
+
 /* selecionando o bloco */
-blocoP.addEventListener('click', function(){
-    block = blocoP.cloneNode();
-    num = 0;
+blocoP.addEventListener("click", function () {
+  block = blocoP.cloneNode();
+  num = 0;
 });
-blocoV.addEventListener('click', function(){
-    block = blocoV.cloneNode();
-    num = 1;
+
+blocoV.addEventListener("click", function () {
+  block = blocoV.cloneNode();
+  num = 1;
 });
-/* colocando o bloco na columna */ 
-for(let i = 0; i <= 6 ;i++){
-    let col = document.getElementById('line' + i);
-    col.addEventListener('click', function(){
-        /* verificando não ter mais de 6 peças na coluna */
-        if(block != '' && (col.childElementCount < 6)){
-            col.appendChild(block);
-        }else {
-            console.log('max')
-        }
-        /* atualizar o array de elementos da coluna */
-        let i = Number(col.id[col.id.length - 1]);
-        for(let j = 0; j < col.childElementCount; j++){ 
-            let nome = col.children[j].id;
-            console.log(nome);
-            if(nome === "Preto"){                
-                arr[i][j] = 1;
-                block = '';
-            }
-            if(nome === "Vermelho"){                
-                arr[i][j] = 2;
-                block = '';
-            }
-        }
-        
 
+/* colocando o bloco na columna */
+for (let i = 0; i <= 6; i++) {
+  let col = document.getElementById("line" + i);
 
+  col.addEventListener("click", function () {
+    /* verificando não ter mais de 6 peças na coluna */
+    if (block != "" && col.childElementCount < 6) {
+      col.appendChild(block);
+    } else {
+      console.log("max");
+    }
 
+    /* atualizar o array de elementos da coluna */
+    let i = Number(col.id[col.id.length - 1]);
+    for (let j = 0; j < col.childElementCount; j++) {
+      let nome = col.children[j].id;
+      if (nome === "Preto") {
+        arr[i][j] = 1;
+        block = "";
+      }
+      if (nome === "Vermelho") {
+        arr[i][j] = 2;
+        block = "";
+      }
+    }
 
-
-
-
-
-        
-        if(linha(arr) || coluna(arr) || diagonal1(arr) ||  diagonal2(arr)){
-            placar.innerHTML = '<Br>' + 'ganhou' + nome;
-        }
-    });
+    if (linha(arr) || coluna(arr) || diagonal1(arr) || diagonal2(arr)) {
+      placar.innerHTML = "<Br>" + "ganhou" + nome;
+    }
+  });
 }
 
-let buttonReset = document.createElement('button');
+let buttonReset = document.createElement("button");
 
-buttonReset.id = 'buttonReset';
+buttonReset.id = "buttonReset";
 
-buttonReset.innerText = 'Reset';
+buttonReset.innerText = "Reset";
 
 container2.appendChild(buttonReset);
 
-let placar = document.createElement('div');
+let placar = document.createElement("div");
 
-placar.id = 'placar';
+placar.id = "placar";
 
-placar.innerText = 'Placar:';
+placar.innerText = "Placar:";
 
 container2.appendChild(placar);
-
-
-
-
-
-
-
 
 // let arrTest = [
 //   [0, 1, 0, 1, 0, 0],
@@ -116,16 +114,14 @@ container2.appendChild(placar);
 //   [0, 1, 0, 1, 0, 0],
 //   [2, 0, 0, 0, 0, 0]
 // ]
-  
-function checkWinDiagonal1(arr){
-  for (let i = 0; i < arr.length -3; i++) {
+
+function checkWinDiagonal1(arr) {
+  for (let i = 0; i < arr.length - 3; i++) {
     for (let j = 0; j < arr[i].length - 3; j++) {
       let count = 1;
 
       if (arr[i][j] > 0) {
-
         for (let t = 1; t < 4; t++) {
-
           if (arr[i][j] === arr[i + t][j + t]) {
             count++;
           }
@@ -134,7 +130,6 @@ function checkWinDiagonal1(arr){
           console.log(`Diagonal Direita ${count}`);
         }
       }
-      
     }
   }
 }
@@ -146,12 +141,10 @@ function checkWinDiagonal2(arr) {
       let count = 1;
 
       if (arr[i][j] > 0) {
-
         for (let t = 1; t < 4; t++) {
-
           if (arr[i][j] === arr[i - t][j + t]) {
             count++;
-          } 
+          }
         }
 
         if (count == 4) {
